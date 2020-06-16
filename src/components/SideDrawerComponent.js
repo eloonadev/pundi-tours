3c4588cbcb7fc60ba4424f7a498dad07006888e2
Bagus Kurnia <baguskurnia1010@gmail.com>
2020-06-16 08:41:02 +0700

    fix bug

--- src/components/SideDrawerComponent.js
+++ src/components/SideDrawerComponent.js
@@ -16,7 +16,7 @@ const sideDrawerComponent = props => {
         <li><Link to="/">WISATA</Link></li>
         <li><Link to="/">LAYANAN LAIN</Link></li>
         <li><Link to="/whyus">MENGAPA KAMI</Link></li>
-        <li><Link to="/galerry">GALLERY</Link></li>
+        <li><Link to="/gallery">GALLERY</Link></li>
         <li><Link to="/">KONTAK</Link></li>
       </ul>
     </nav>
