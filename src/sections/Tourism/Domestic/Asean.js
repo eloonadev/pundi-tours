0eb0e4dd914cca26a459920568654207f129dc5e
Bagus Kurnia <baguskurnia1010@gmail.com>
2020-07-02 19:00:29 +0700

    fix issue

--- src/sections/Tourism/Domestic/Asean.js
+++ src/sections/Tourism/Domestic/Asean.js
@@ -9,7 +9,7 @@ const Asean = () => (
           <li className="grid-service--list">
             <div className="card-wrapper">
               <img src={require("../../../assets/tour_indonesia/BATAM.jpg")} style={{ width: "240px", height: "200px" }} alt="Al Kaffah" />
-              <a href="https://drive.google.com/file/d/1nUJ6_6ET1wTR2cGHUQEgI2rGWw6EWCen/view?usp=sharing" className="title-wrapper" target="_blank" rel="nooponer noreferrer">
+              <a href="https://drive.google.com/file/d/1nUJ6_6ET1wTR2cGHUQEgI2rGWw6EWCen/view?usp=sharing" className="title-wrapper" target="_blank" rel="noopener noreferrer">
                 <p>Batam - Bintan</p>
                 <i className="fas fa-chevron-right"></i>
               </a>
@@ -18,7 +18,7 @@ const Asean = () => (
           <li className="grid-service--list">
             <div className="card-wrapper">
               <img src={require("../../../assets/tour_indonesia/ACEH.jpg")} style={{ width: "240px", height: "200px" }} alt="Al Kaffah" />
-              <a href="https://drive.google.com/file/d/1nUJ6_6ET1wTR2cGHUQEgI2rGWw6EWCen/view?usp=sharing" className="title-wrapper" target="_blank" rel="nooponer noreferrer">
+              <a href="https://drive.google.com/file/d/1nUJ6_6ET1wTR2cGHUQEgI2rGWw6EWCen/view?usp=sharing" className="title-wrapper" target="_blank" rel="noopener noreferrer">
                 <p>Aceh</p>
                 <i className="fas fa-chevron-right"></i>
               </a>
@@ -27,7 +27,7 @@ const Asean = () => (
           <li className="grid-service--list">
             <div className="card-wrapper">
               <img src={require("../../../assets/tour_indonesia/BALI.jpg")} style={{ width: "240px", height: "200px" }} alt="Al Kaffah" />
-              <a href="https://drive.google.com/file/d/1qksXSbH2trSQr3eGCYlRRaVacQpzzxdb/view?usp=sharing" className="title-wrapper" target="_blank" rel="nooponer noreferrer">
+              <a href="https://drive.google.com/file/d/1qksXSbH2trSQr3eGCYlRRaVacQpzzxdb/view?usp=sharing" className="title-wrapper" target="_blank" rel="noopener noreferrer">
                 <p>Bali</p>
                 <i className="fas fa-chevron-right"></i>
               </a>
@@ -36,7 +36,7 @@ const Asean = () => (
           <li className="grid-service--list">
             <div className="card-wrapper">
               <img src={require("../../../assets/tour_indonesia/BELITUNG.jpg")} style={{ width: "240px", height: "200px" }} alt="Al Kaffah" />
-              <a href="https://drive.google.com/file/d/19mLJZ_qOZbwAKs4lZyujXjzhN54X05eM/view?usp=sharing" className="title-wrapper" target="_blank" rel="nooponer noreferrer">
+              <a href="https://drive.google.com/file/d/19mLJZ_qOZbwAKs4lZyujXjzhN54X05eM/view?usp=sharing" className="title-wrapper" target="_blank" rel="noopener noreferrer">
                 <p>Belitung</p>
                 <i className="fas fa-chevron-right"></i>
               </a>
@@ -45,7 +45,7 @@ const Asean = () => (
           <li className="grid-service--list">
             <div className="card-wrapper">
               <img src={require("../../../assets/tour_indonesia/BENGKULU.jpg")} style={{ width: "240px", height: "200px" }} alt="Al Kaffah" />
-              <a href="https://drive.google.com/file/d/1aOHqFXXtx2HNXGfjbjEZhJlxeoxxfUtI/view?usp=sharing" className="title-wrapper" target="_blank" rel="nooponer noreferrer">
+              <a href="https://drive.google.com/file/d/1aOHqFXXtx2HNXGfjbjEZhJlxeoxxfUtI/view?usp=sharing" className="title-wrapper" target="_blank" rel="noopener noreferrer">
                 <p>Bengkulu</p>
                 <i className="fas fa-chevron-right"></i>
               </a>
@@ -54,7 +54,7 @@ const Asean = () => (
           <li className="grid-service--list">
             <div className="card-wrapper">
               <img src={require("../../../assets/tour_indonesia/JOGJA.jpg")} style={{ width: "240px", height: "200px" }} alt="Al Kaffah" />
-              <a href="https://drive.google.com/file/d/1aOHqFXXtx2HNXGfjbjEZhJlxeoxxfUtI/view?usp=sharing" className="title-wrapper" target="_blank" rel="nooponer noreferrer">
+              <a href="https://drive.google.com/file/d/1aOHqFXXtx2HNXGfjbjEZhJlxeoxxfUtI/view?usp=sharing" className="title-wrapper" target="_blank" rel="noopener noreferrer">
                 <p>Jogja</p>
                 <i className="fas fa-chevron-right"></i>
               </a>
@@ -63,7 +63,7 @@ const Asean = () => (
           <li className="grid-service--list">
             <div className="card-wrapper">
               <img src={require("../../../assets/tour_indonesia/LOMBOK.jpg")} style={{ width: "240px", height: "200px" }} alt="Al Kaffah" />
-              <a href="https://drive.google.com/file/d/1yVfrUisXocx-dnTHqexgqXChm2_y_HmO/view?usp=sharing" className="title-wrapper" target="_blank" rel="nooponer noreferrer">
+              <a href="https://drive.google.com/file/d/1yVfrUisXocx-dnTHqexgqXChm2_y_HmO/view?usp=sharing" className="title-wrapper" target="_blank" rel="noopener noreferrer">
                 <p>Lombok</p>
                 <i className="fas fa-chevron-right"></i>
               </a>
@@ -72,7 +72,7 @@ const Asean = () => (
           <li className="grid-service--list">
             <div className="card-wrapper">
               <img src={require("../../../assets/tour_indonesia/MALANG.jpg")} style={{ width: "240px", height: "200px" }} alt="Al Kaffah" />
-              <a href="https://drive.google.com/file/d/1yVfrUisXocx-dnTHqexgqXChm2_y_HmO/view?usp=sharing" className="title-wrapper" target="_blank" rel="nooponer noreferrer">
+              <a href="https://drive.google.com/file/d/1yVfrUisXocx-dnTHqexgqXChm2_y_HmO/view?usp=sharing" className="title-wrapper" target="_blank" rel="noopener noreferrer">
                 <p>Malang</p>
                 <i className="fas fa-chevron-right"></i>
               </a>
@@ -81,7 +81,7 @@ const Asean = () => (
           <li className="grid-service--list">
             <div className="card-wrapper">
               <img src={require("../../../assets/tour_indonesia/MANADO.jpg")} style={{ width: "240px", height: "200px" }} alt="Al Kaffah" />
-              <a href="https://drive.google.com/file/d/1yVfrUisXocx-dnTHqexgqXChm2_y_HmO/view?usp=sharing" className="title-wrapper" target="_blank" rel="nooponer noreferrer">
+              <a href="https://drive.google.com/file/d/1yVfrUisXocx-dnTHqexgqXChm2_y_HmO/view?usp=sharing" className="title-wrapper" target="_blank" rel="noopener noreferrer">
                 <p>Manado</p>
                 <i className="fas fa-chevron-right"></i>
               </a>
@@ -90,7 +90,7 @@ const Asean = () => (
           <li className="grid-service--list">
             <div className="card-wrapper">
               <img src={require("../../../assets/tour_indonesia/MEDAN.jpg")} style={{ width: "240px", height: "200px" }} alt="Al Kaffah" />
-              <a href="https://drive.google.com/file/d/18E6diO9f-6pkT0scJADV4MixjQBctMlc/view?usp=sharing" className="title-wrapper" target="_blank" rel="nooponer noreferrer">
+              <a href="https://drive.google.com/file/d/18E6diO9f-6pkT0scJADV4MixjQBctMlc/view?usp=sharing" className="title-wrapper" target="_blank" rel="noopener noreferrer">
                 <p>Medan</p>
                 <i className="fas fa-chevron-right"></i>
               </a>
@@ -99,7 +99,7 @@ const Asean = () => (
           <li className="grid-service--list">
             <div className="card-wrapper">
               <img src={require("../../../assets/tour_indonesia/PADANG.jpg")} style={{ width: "240px", height: "200px" }} alt="Al Kaffah" />
-              <a href="https://drive.google.com/file/d/18E6diO9f-6pkT0scJADV4MixjQBctMlc/view?usp=sharing" className="title-wrapper" target="_blank" rel="nooponer noreferrer">
+              <a href="https://drive.google.com/file/d/18E6diO9f-6pkT0scJADV4MixjQBctMlc/view?usp=sharing" className="title-wrapper" target="_blank" rel="noopener noreferrer">
                 <p>Padang</p>
                 <i className="fas fa-chevron-right"></i>
               </a>
@@ -108,7 +108,7 @@ const Asean = () => (
           <li className="grid-service--list">
             <div className="card-wrapper">
               <img src={require("../../../assets/tour_indonesia/PALEMBANG.jpg")} style={{ width: "240px", height: "200px" }} alt="Al Kaffah" />
-              <a href="https://drive.google.com/file/d/1J0wN18-2K3Oqo1Y9Keun-w46C9hFt0Lu/view?usp=sharing" className="title-wrapper" target="_blank" rel="nooponer noreferrer">
+              <a href="https://drive.google.com/file/d/1J0wN18-2K3Oqo1Y9Keun-w46C9hFt0Lu/view?usp=sharing" className="title-wrapper" target="_blank" rel="noopener noreferrer">
                 <p>Palembang</p>
                 <i className="fas fa-chevron-right"></i>
               </a>
--- src/sections/Tourism/Domestic/Foreign.js
+++ src/sections/Tourism/Domestic/Foreign.js
@@ -9,7 +9,7 @@ const Foreign = () => (
           <li className="grid-service--list">
             <div className="card-wrapper">
               <img src={require("../../../assets/tour_indonesia/BATAM.jpg")} style={{ width: "240px", height: "200px" }} alt="Al Kaffah" />
-              <a href="https://drive.google.com/file/d/1Kz9jPVJH2dxmnzLVGZyMi_37WCv7wAC1/view?usp=sharing" className="title-wrapper" target="_blank" rel="nooponer noreferrer">
+              <a href="https://drive.google.com/file/d/1Kz9jPVJH2dxmnzLVGZyMi_37WCv7wAC1/view?usp=sharing" className="title-wrapper" target="_blank" rel="noopener noreferrer">
                 <p>Batam - Bintan</p>
                 <i className="fas fa-chevron-right"></i>
               </a>
@@ -18,7 +18,7 @@ const Foreign = () => (
           <li className="grid-service--list">
             <div className="card-wrapper">
               <img src={require("../../../assets/tour_indonesia/ACEH.jpg")} style={{ width: "240px", height: "200px" }} alt="Al Kaffah" />
-              <a href="https://drive.google.com/file/d/1XSB7u9Rv3Skz0DpanB8PEOF0l3T0IsKE/view?usp=sharing" className="title-wrapper" target="_blank" rel="nooponer noreferrer">
+              <a href="https://drive.google.com/file/d/1XSB7u9Rv3Skz0DpanB8PEOF0l3T0IsKE/view?usp=sharing" className="title-wrapper" target="_blank" rel="noopener noreferrer">
                 <p>Aceh</p>
                 <i className="fas fa-chevron-right"></i>
               </a>
@@ -27,7 +27,7 @@ const Foreign = () => (
           <li className="grid-service--list">
             <div className="card-wrapper">
               <img src={require("../../../assets/tour_indonesia/BALI.jpg")} style={{ width: "240px", height: "200px" }} alt="Al Kaffah" />
-              <a href="https://drive.google.com/file/d/1MPzrAV2iKfLWvQJS-nFDbv9WH0xCih79/view?usp=sharing" className="title-wrapper" target="_blank" rel="nooponer noreferrer">
+              <a href="https://drive.google.com/file/d/1MPzrAV2iKfLWvQJS-nFDbv9WH0xCih79/view?usp=sharing" className="title-wrapper" target="_blank" rel="noopener noreferrer">
                 <p>Bali</p>
                 <i className="fas fa-chevron-right"></i>
               </a>
@@ -36,7 +36,7 @@ const Foreign = () => (
           <li className="grid-service--list">
             <div className="card-wrapper">
               <img src={require("../../../assets/tour_indonesia/BELITUNG.jpg")} style={{ width: "240px", height: "200px" }} alt="Al Kaffah" />
-              <a href="https://drive.google.com/file/d/1AmAJOBxmK7hPPAnhpoCETyWp47laXC0U/view?usp=sharing" className="title-wrapper" target="_blank" rel="nooponer noreferrer">
+              <a href="https://drive.google.com/file/d/1AmAJOBxmK7hPPAnhpoCETyWp47laXC0U/view?usp=sharing" className="title-wrapper" target="_blank" rel="noopener noreferrer">
                 <p>Belitung</p>
                 <i className="fas fa-chevron-right"></i>
               </a>
@@ -45,7 +45,7 @@ const Foreign = () => (
           <li className="grid-service--list">
             <div className="card-wrapper">
               <img src={require("../../../assets/tour_indonesia/BENGKULU.jpg")} style={{ width: "240px", height: "200px" }} alt="Al Kaffah" />
-              <a href="https://drive.google.com/file/d/1Gd6Jxnw_8mE4zPAiQKCm_9CD7sEh0rx4/view?usp=sharing" className="title-wrapper" target="_blank" rel="nooponer noreferrer">
+              <a href="https://drive.google.com/file/d/1Gd6Jxnw_8mE4zPAiQKCm_9CD7sEh0rx4/view?usp=sharing" className="title-wrapper" target="_blank" rel="noopener noreferrer">
                 <p>Bengkulu</p>
                 <i className="fas fa-chevron-right"></i>
               </a>
@@ -54,7 +54,7 @@ const Foreign = () => (
           <li className="grid-service--list">
             <div className="card-wrapper">
               <img src={require("../../../assets/tour_indonesia/JOGJA.jpg")} style={{ width: "240px", height: "200px" }} alt="Al Kaffah" />
-              <a href="https://drive.google.com/file/d/1cj-pzUHgicdam-UFR88i1YZ-teSoBnEC/view?usp=sharing" className="title-wrapper" target="_blank" rel="nooponer noreferrer">
+              <a href="https://drive.google.com/file/d/1cj-pzUHgicdam-UFR88i1YZ-teSoBnEC/view?usp=sharing" className="title-wrapper" target="_blank" rel="noopener noreferrer">
                 <p>Jogja</p>
                 <i className="fas fa-chevron-right"></i>
               </a>
@@ -63,7 +63,7 @@ const Foreign = () => (
           <li className="grid-service--list">
             <div className="card-wrapper">
               <img src={require("../../../assets/tour_indonesia/LOMBOK.jpg")} style={{ width: "240px", height: "200px" }} alt="Al Kaffah" />
-              <a href="https://drive.google.com/file/d/11cOkxgdFKvWhDHww6aZpCDx_DdYrnXlT/view?usp=sharing" className="title-wrapper" target="_blank" rel="nooponer noreferrer">
+              <a href="https://drive.google.com/file/d/11cOkxgdFKvWhDHww6aZpCDx_DdYrnXlT/view?usp=sharing" className="title-wrapper" target="_blank" rel="noopener noreferrer">
                 <p>Lombok</p>
                 <i className="fas fa-chevron-right"></i>
               </a>
@@ -72,7 +72,7 @@ const Foreign = () => (
           <li className="grid-service--list">
             <div className="card-wrapper">
               <img src={require("../../../assets/tour_indonesia/MALANG.jpg")} style={{ width: "240px", height: "200px" }} alt="Al Kaffah" />
-              <a href="https://drive.google.com/file/d/1snIXq8G7S4-ChYQZoNOKwI4RnM6MJpCP/view?usp=sharing" className="title-wrapper" target="_blank" rel="nooponer noreferrer">
+              <a href="https://drive.google.com/file/d/1snIXq8G7S4-ChYQZoNOKwI4RnM6MJpCP/view?usp=sharing" className="title-wrapper" target="_blank" rel="noopener noreferrer">
                 <p>Malang Raya</p>
                 <i className="fas fa-chevron-right"></i>
               </a>
@@ -81,7 +81,7 @@ const Foreign = () => (
           <li className="grid-service--list">
             <div className="card-wrapper">
               <img src={require("../../../assets/tour_indonesia/MANADO.jpg")} style={{ width: "240px", height: "200px" }} alt="Al Kaffah" />
-              <a href="https://drive.google.com/file/d/1k3p5ywshZKD1FRwYLW8hth0M7zHeUDCg/view?usp=sharing" className="title-wrapper" target="_blank" rel="nooponer noreferrer">
+              <a href="https://drive.google.com/file/d/1k3p5ywshZKD1FRwYLW8hth0M7zHeUDCg/view?usp=sharing" className="title-wrapper" target="_blank" rel="noopener noreferrer">
                 <p>Manado</p>
                 <i className="fas fa-chevron-right"></i>
               </a>
@@ -90,7 +90,7 @@ const Foreign = () => (
           <li className="grid-service--list">
             <div className="card-wrapper">
               <img src={require("../../../assets/tour_indonesia/MEDAN.jpg")} style={{ width: "240px", height: "200px" }} alt="Al Kaffah" />
-              <a href="https://drive.google.com/file/d/130tmMAQ1q4gnQ5ZkreQF3mjgFYEByRWn/view?usp=sharing" className="title-wrapper" target="_blank" rel="nooponer noreferrer">
+              <a href="https://drive.google.com/file/d/130tmMAQ1q4gnQ5ZkreQF3mjgFYEByRWn/view?usp=sharing" className="title-wrapper" target="_blank" rel="noopener noreferrer">
                 <p>Medan</p>
                 <i className="fas fa-chevron-right"></i>
               </a>
@@ -99,7 +99,7 @@ const Foreign = () => (
           <li className="grid-service--list">
             <div className="card-wrapper">
               <img src={require("../../../assets/tour_indonesia/PADANG.jpg")} style={{ width: "240px", height: "200px" }} alt="Al Kaffah" />
-              <a href="https://drive.google.com/file/d/1FG0KmtBHfFo_KmGB02o7ElcJ3MTVv6lA/view?usp=sharing" className="title-wrapper" target="_blank" rel="nooponer noreferrer">
+              <a href="https://drive.google.com/file/d/1FG0KmtBHfFo_KmGB02o7ElcJ3MTVv6lA/view?usp=sharing" className="title-wrapper" target="_blank" rel="noopener noreferrer">
                 <p>Padang</p>
                 <i className="fas fa-chevron-right"></i>
               </a>
@@ -108,7 +108,7 @@ const Foreign = () => (
           <li className="grid-service--list">
             <div className="card-wrapper">
               <img src={require("../../../assets/tour_indonesia/PALEMBANG.jpg")} style={{ width: "240px", height: "200px" }} alt="Al Kaffah" />
-              <a href="https://drive.google.com/file/d/13mEDcMvbYKYZ7iH2HiGCYkx0H52RU9Tt/view?usp=sharing" className="title-wrapper" target="_blank" rel="nooponer noreferrer">
+              <a href="https://drive.google.com/file/d/13mEDcMvbYKYZ7iH2HiGCYkx0H52RU9Tt/view?usp=sharing" className="title-wrapper" target="_blank" rel="noopener noreferrer">
                 <p>Palembang</p>
                 <i className="fas fa-chevron-right"></i>
               </a>
